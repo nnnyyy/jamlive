449c835e785ffc0efb094aa1e8674609be70fe62
BruceChae <newwodudrj@gmail.com>
2018-05-10 10:28:11 +0900

    Auto stash before merge of "master" and "origin/master"

--- jamlivevote_web/WebSocketHandler.ashx.cs
+++ jamlivevote_web/WebSocketHandler.ashx.cs
@@ -1,20 +1,31 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-using System.Web.WebSockets;
 using System.Net.WebSockets;
 using System.Text;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Collections.Concurrent;
+using System.Web;
+using System.Web.WebSockets;
+using Newtonsoft.Json;
 
 namespace MabiWebSocket
 {
+    internal class ClickCountMessage
+    {
+        public long Time { get; set; }
+        public Dictionary<string, int> Count { get; set; }
+    }
+
     public class WebSocketHandler : HttpTaskAsyncHandler
     {
-        public static IDictionary<string, WebSocket> Sockets = new Dictionary<string, WebSocket>();
+        static readonly int AccumlativeSec = 10;
+        static readonly int CooldownSec = 1;
+
+        static Dictionary<int, WebSocket> WebSocketDic = new Dictionary<int, WebSocket>();
+        static Dictionary<int, long> LastClickTimeDic = new Dictionary<int, long>();
+        static Dictionary<long, Dictionary<string, int>> TimeSeriesCountDic = new Dictionary<long, Dictionary<string, int>>();
+
         public override bool IsReusable
         {
             get
@@ -23,81 +34,119 @@ namespace MabiWebSocket
             }
         }
 
-        //Socket Object, Although i have created a Static Dictionary of Scoket objects just to show the sample working. What i do is create this Socket object for each user and 
-        //keeps it into the dictionary. You can obviously change the implementation in real time.
-        private WebSocket Socket { get; set; }
-
-        //Overriden menthod Process Request async/await featur has been used.
         public override async Task ProcessRequestAsync(HttpContext httpContext)
         {
-            //task is executed
             await Task.Run(() =>
             {
-                //Checks if it is a Web Socket Request
                 if (httpContext.IsWebSocketRequest)
                 {
-                    httpContext.AcceptWebSocketRequest(async delegate (AspNetWebSocketContext aspNetWebSocketContext)
-                    {
-                        Socket = aspNetWebSocketContext.WebSocket;
-
-                        //Checks if the connection is not already closed
-                        while (Socket != null || Socket.State != WebSocketState.Closed)
-                        {
-                            //Recieves the message from client
-                            ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[1024]);
-                            WebSocketReceiveResult webSocketReceiveResult = await Socket.ReceiveAsync(buffer, CancellationToken.None);
-
-                            //Here i have handled the case of text based communication, you can also put down your hode to handle byte arrays etc.
-                            switch (webSocketReceiveResult.MessageType)
-                            {
-                                case WebSocketMessageType.Text:
-                                    OnMessageReceived(Encoding.UTF8.GetString(buffer.Array, 0, webSocketReceiveResult.Count));
-                                    break;
-                            }
-                        }
-                    });
+                    httpContext.AcceptWebSocketRequest(ProcessWebSocketRequest);
                 }
             });
         }
 
-        //Sends message to the client
-        private async Task SendMessageAsync(string message, WebSocket socket)
+        private async Task ProcessWebSocketRequest(AspNetWebSocketContext aspNetWebSocketContext)
         {
-            await SendMessageAsync(Encoding.UTF8.GetBytes(message), socket);
+            var webSocket = aspNetWebSocketContext.WebSocket;
+            var webSocketKey = webSocket.GetHashCode();
+
+            while (webSocket != null && webSocket.State != WebSocketState.Closed)
+            {
+                WebSocketDic[webSocketKey] = webSocket;
+
+                ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[1024]);
+                WebSocketReceiveResult webSocketReceiveResult = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
+
+                if (webSocket.State == WebSocketState.Open)
+                {
+                    var message = Encoding.UTF8.GetString(buffer.Array, 0, webSocketReceiveResult.Count);
+                    OnMessageReceived(webSocket, message);
+                }
+                else
+                {
+                    WebSocketDic.Remove(webSocketKey);
+                }
+            }
         }
 
-        //Sends the message to the client
-        private async Task SendMessageAsync(byte[] message, WebSocket socket)
+        //This message is fired and parent can forget about this, what this method do is gets the message and push it to the different clients which are connected
+        void OnMessageReceived(WebSocket webSocket, string message)
         {
-            await socket.SendAsync(
-                new ArraySegment<byte>(message),
+            var webSocketKey = webSocket.GetHashCode();
+            var clickCountKey = message;
+            var now = DateTime.Now.ToYYYYMMDDHHMMSS();
+
+            // 쿨타임 체크
+            if (LastClickTimeDic.ContainsKey(webSocketKey) && LastClickTimeDic[webSocketKey] + CooldownSec <= now)
+            {
+                return;
+            }
+            LastClickTimeDic[webSocketKey] = now;
+
+            // 시간 갱신
+            if (TimeSeriesCountDic.ContainsKey(now) == false)
+            {
+                TimeSeriesCountDic.Add(now, new Dictionary<string, int>());
+                TimeSeriesCountDic = TimeSeriesCountDic.Skip(Math.Max(0, TimeSeriesCountDic.Count() - AccumlativeSec)).ToDictionary(x => x.Key, x => x.Value);
+            }
+
+            // 클릭 횟수 증가
+            TimeSeriesCountDic[now].AddOrIncrease(clickCountKey, 1);
+
+            BroadcastClickCount();
+        }
+
+        private void BroadcastClickCount()
+        {
+            if (TimeSeriesCountDic.Count() > 0)
+            {
+                var clickCountMessage = new ClickCountMessage();
+
+                clickCountMessage.Time = TimeSeriesCountDic.Last().Key;
+
+                foreach (var clickCountDic in TimeSeriesCountDic.Values)
+                {
+                    foreach (var clickCount in clickCountDic)
+                    {
+                        clickCountMessage.Count.AddOrIncrease(clickCount.Key, clickCount.Value);
+                    }
+                }
+
+                var jsonString = JsonConvert.SerializeObject(clickCountMessage);
+
+                foreach (var webSocket in WebSocketDic.Values)
+                {
+                    SendAsyncMessage(webSocket, jsonString);
+                }
+            }
+        }
+
+        async void SendAsyncMessage(WebSocket webSocket, string message)
+        {
+            await webSocket.SendAsync(
+                new ArraySegment<byte>(Encoding.UTF8.GetBytes(message)),
                 WebSocketMessageType.Text,
                 true,
                 CancellationToken.None);
         }
+    }
 
-        //This message is fired and parent can forget about this, what this method do is gets the message and push it to the different clients which are connected
-        protected void OnMessageReceived(string message)
+    static class Extension
+    {
+        public static long ToYYYYMMDDHHMMSS(this DateTime time)
         {
-            Task task;
+            return Convert.ToInt64(time.ToString("yyyyMMddHHmmss"));
+        }
 
-            if (message.IndexOf("JOINEDSAMPLECHAT") == 0)
+        public static void AddOrIncrease<T>(this Dictionary<T, int> dic, T key, int value)
+        {
+            if (dic.ContainsKey(key))
             {
-                Sockets[message.Replace("JOINEDSAMPLECHAT:", string.Empty)] = Socket;
-                foreach (string key in Sockets.Keys)
-                {
-                    task = SendMessageAsync(string.Concat(message.Replace("JOINEDSAMPLECHAT:", string.Empty), " Joined Chat."), Sockets[key]);
-                }
+                dic.Add(key, value);
             }
             else
             {
-                if (message.IndexOf("BROADCAST") == 0)
-                {
-                    foreach (string key in Sockets.Keys)
-                    {
-                        task = SendMessageAsync(message.Replace("BROADCAST:", string.Empty), Sockets[key]);
-                    }
-                }
+                dic[key] += value;
             }
         }
     }
