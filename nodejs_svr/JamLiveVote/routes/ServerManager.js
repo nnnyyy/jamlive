d8cb5abdbde877354d9e968b8fd765cc2a6d107d
nnnyyy <nnnyyy@nate.com>
2018-05-11 10:23:39 +0900

    Auto stash before merge of "master" and "origin/master"

--- nodejs_svr/JamLiveVote/routes/ServerManager.js
+++ nodejs_svr/JamLiveVote/routes/ServerManager.js
@@ -41,37 +41,39 @@ ServerMan.prototype.setIO = function(io) {
 ServerMan.prototype.broadcastVoteInfo = function() {
     var cur = new Date();
     cur /= 1000;
-    cur -= (cur % 5);
+    cur -= (cur % 3);
 
     if( this.counts.get(cur) == null ) {
-        this.counts.set(cur, 0);
+        this.counts.set(cur, [0,0,0]);
         this.countslist.push(cur);
         if( this.counts.count() > 10 ) {
             this.counts.delete(this.countslist[0]);
             this.countslist.shift();
         }
     }
+
+    //console.log(this.counts);
 
     this.io.sockets.emit('testdata', {cnt: JSON.stringify(this.counts)})
 }
 
-ServerMan.prototype.click = function() {
+ServerMan.prototype.click = function(idx) {
     var cur = new Date();
     cur /= 1000;
-    cur -= (cur % 5);
+    cur -= (cur % 3);
     var obj = this.counts.get(cur);
     if( obj == null ) {
-        this.counts.set(cur, 1);
+        this.counts.set(cur, [0,0,0]);
         this.countslist.push(cur);
         if( this.counts.count() > 10 ) {
             this.counts.delete(this.countslist[0]);
             this.countslist.shift();
         }
+        obj = this.counts.get(cur);
     }
-    else {
-        obj++;
-        this.counts.set(cur, obj);
-    }
+
+    if( obj == null ) return;
+    obj[idx]++;
 }
 
 ServerMan.prototype.register = function(socket) {
@@ -80,10 +82,10 @@ ServerMan.prototype.register = function(socket) {
         servman.removeSocket(this);
     });
 
-    socket.on('vote', function() {
+    socket.on('vote', function(data) {
         var client = servman.getClient(this)
         if( client.isClickable() ) {
-            servman.click();
+            servman.click(data.idx);
             client.tLastClick = new Date();
         }
     })
