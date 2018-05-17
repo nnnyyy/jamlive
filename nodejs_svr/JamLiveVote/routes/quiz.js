d3d2bd1adc631e5cc50be8aa231228322405807d
nnnyyy <nnnyyy@nate.com>
2018-05-17 17:11:18 +0900

    잼라이브 퀴즈 만드는 중

---
+++ nodejs_svr/JamLiveVote/routes/quiz.js
@@ -0,0 +1,20 @@
+/**
+ * Created by nnnyyy on 2018-05-17.
+ */
+/**
+ * Created by nnnyyy on 2018-04-13.
+ */
+var express = require('express');
+var router = express.Router();
+var dbhelper = require('./dbhelper');
+var moment = require('moment');
+var Log = require('./Log');
+
+router.get('/', function(req, res, next) {
+    dbhelper.getQuizDateList(function(result) {
+        result.moment = moment;
+        res.render('quiz/index', result);
+    });
+});
+
+module.exports = router;
