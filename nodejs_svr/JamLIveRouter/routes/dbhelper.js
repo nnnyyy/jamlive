c5503827d3f57b9386cbce8c98a4815448b42cea
nnnyyy <nnnyyy@nate.com>
2018-06-18 16:44:27 +0900

    라우터

---
+++ nodejs_svr/JamLIveRouter/routes/dbhelper.js
@@ -0,0 +1,85 @@
+/**
+ * Created by nnnyy on 2018-04-13.
+ */
+var Log = require('./Log');
+var dbpool = require('./MySQL').init();
+
+exports.getQuizDateList = function(cb) {
+    try {
+        dbpool.query("select sn, quiz_date from quiz_date_list order by quiz_date", function(err, rows) {
+            if(err) {
+                cb({ret: -1});
+                return;
+            }
+            var data = [];
+            for( var i  = 0; i < rows.length ; ++i ) {
+                var d = rows[i];
+                data.push({sn: d.sn, quiz_date: d.quiz_date});
+            }
+            cb({ret:0, list: data});
+        });
+    }catch(err) {
+        Log.logger.debug('DB Failed - getQuizDateList');
+        cb({ret: -1});
+    }
+}
+
+exports.getQuizList = function(sn, cb) {
+    try {
+        dbpool.query("select * from quiz where date_sn = " + sn + ' order by quiz_idx', function(err, rows) {
+            if(err) {
+                cb({ret: -1});
+                return;
+            }
+            var data = [];
+            for( var i  = 0; i < rows.length ; ++i ) {
+                var d = rows[i];
+                data.push({idx: d.quiz_idx, question: d.question ,answer: [d.answer1, d.answer2, d.answer3], collect: d.collect_idx});
+            }
+            cb({ret:0, quizlist: data});
+        });
+    }catch(err) {
+        Log.logger.debug('DB Failed - getQuizDateList');
+        cb({ret: -1});
+    }
+}
+
+exports.getRandomQuizList = function( cb ) {
+    try {
+        dbpool.query("select * from quiz where quiz_idx >= 4 order by rand() limit 0,12", function(err, rows) {
+            if(err) {
+                cb({ret: -1});
+                return;
+            }
+            var data = [];
+            for( var i  = 0; i < rows.length ; ++i ) {
+                var d = rows[i];
+                data.push({idx: d.quiz_idx, question: d.question ,answer: [d.answer1, d.answer2, d.answer3], collect: d.collect_idx});
+            }
+            cb({ret:0, quizlist: data});
+        });
+    }catch(err) {
+        Log.logger.debug('DB Failed - getQuizDateList');
+        cb({ret: -1});
+    }
+}
+
+exports.getRandomQuiz = function( cb ) {
+    try {
+        dbpool.query("select * from quiz where quiz_idx >= 4 order by rand() limit 0,1", function(err, rows) {
+            if(err) {
+                cb({ret: -1});
+                return;
+            }
+            var data = [];
+            for( var i  = 0; i < rows.length ; ++i ) {
+                var d = rows[i];
+                data.push({idx: d.quiz_idx, question: d.question ,answer: [d.answer1, d.answer2, d.answer3], collect: d.collect_idx});
+            }
+            cb({ret:0, quizdata: data[0]});
+        });
+    }catch(err) {
+        Log.logger.debug('DB Failed - getQuizDateList');
+        cb({ret: -1});
+    }
+}
