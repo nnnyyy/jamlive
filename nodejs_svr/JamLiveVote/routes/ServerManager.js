29a3aa67cc3f401e91e4c9f6e8e0f60ae87d881a
nnnyyy <nnnyyy@nate.com>
2018-07-30 12:02:50 +0900

    서버 기준 수정

--- nodejs_svr/JamLiveVote/routes/ServerManager.js
+++ nodejs_svr/JamLiveVote/routes/ServerManager.js
@@ -386,7 +386,7 @@ ServerMan.prototype.register = function(socket) {
 
     client.nick = nick;
 
-    if( this.socketmap.count() > 400 && ( !client.isLogined() || socket.request.session.auth <= 1 ) ) {
+    if( this.socketmap.count() > 500 && ( !client.isLogined() || socket.request.session.auth <= 1 ) ) {
         socket.emit('reconn-server', {url: 'ch2.jamlive.net'});
         return;
     }
