568fd22ee2917221e3304c28cd85f30455caad4f
nnnyyy <nnnyyy@nate.com>
2018-08-05 16:51:57 +0900

    ㅇㅇ

--- nodejs_svr/JamLiveVote/routes/jamlive.js
+++ nodejs_svr/JamLiveVote/routes/jamlive.js
@@ -274,8 +274,8 @@ exports.searchex = function(req, res, next) {
         api_url += '&sort=sim';
     }
 
-    var clientids = ['zGJt30deH5ozVHAtGvu9', 'RrVyoeWlAzqS736WZDq3', 'V074_asyyV_2Etx5ZtLW', 'niwBM2EN40JlAgR2_B1B', 'UFEvdYw_RtvqrxVNKlYl', 'NhTno6hxnZpTGZUPffvI', 'x8DQ2xYWaeQqVGDZzdL4'];
-    var secrets = ['kkusj_izbs', 'ZaMzW0bOM7', 'NCybd8sKXd', 'AltOR9YRrw', 'mKKbFNGP1G', 'foM4QTYU9U', 'vPCM314sAS'];
+    var clientids = ['VZ9vuXcJh36t7IuDAeCW','zGJt30deH5ozVHAtGvu9', 'RrVyoeWlAzqS736WZDq3', 'V074_asyyV_2Etx5ZtLW', 'niwBM2EN40JlAgR2_B1B', 'UFEvdYw_RtvqrxVNKlYl', 'NhTno6hxnZpTGZUPffvI', 'x8DQ2xYWaeQqVGDZzdL4'];
+    var secrets = ['cP_ecRsKYR','kkusj_izbs', 'ZaMzW0bOM7', 'NCybd8sKXd', 'AltOR9YRrw', 'mKKbFNGP1G', 'foM4QTYU9U', 'vPCM314sAS'];
     var modcnt = clientids.length;
 
 
