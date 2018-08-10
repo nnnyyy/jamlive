3f1ec222c362f444f291ab2659a45f64746a72b6
nnnyyy <nnnyyy@nate.com>
2018-08-10 17:12:08 +0900

    Admob 광고 넣기 .. ing

--- LiveQuizShareApp/app/src/main/java/net/jamlive/livequizshare/MainActivity.java
+++ LiveQuizShareApp/app/src/main/java/net/jamlive/livequizshare/MainActivity.java
@@ -8,10 +8,24 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 import net.jamlive.livequizshare.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity {
     ActivityMainBinding mBinding;
+    private AdView mAdView;
+    private AdListener AdmobAdListener = new AdListener() {
+        @Override
+        public void onAdLoaded() {
+            super.onAdLoaded();
+            Log.i("Admob Banner Ads","onAdLoaded");
+        }
+    };
+
     View.OnClickListener onBtnServiceStartListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -35,5 +49,16 @@ public class MainActivity extends Activity {
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         mBinding.btnServiceStart.setOnClickListener(onBtnServiceStartListener);
         mBinding.btnServiceStop.setOnClickListener(onBtnServiceStopListener);
+
+        //MobileAds.initialize(this, getResources().getString(R.string.admob_app_id));
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+
+        mBinding.adView.setAdListener(AdmobAdListener);
+
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        mBinding.adView.loadAd(adRequest);
     }
 }
