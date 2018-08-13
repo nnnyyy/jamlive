cc0be9bdf929723bda675eee738dec659e9386e1
nnnyyy <nnnyyy@nate.com>
2018-08-14 01:15:27 +0900

    결합게임 베이스

---
+++ nodejs_svr/JamLiveVote/routes/modules/SetGameMan.js
@@ -0,0 +1,44 @@
+/**
+ * Created by nnnyy on 2018-08-14.
+ */
+'use strict'
+
+class SetGameMan {
+    constructor() {
+
+    }
+
+    isRunning() {
+        return false;
+    }
+
+    makeGame(client) {
+        return true;
+    }
+
+    startGame() {
+
+    }
+
+    shuffleCard() {
+
+    }
+
+    popList() {
+        
+    }
+
+    update() {
+
+    }
+
+    stop() {
+
+    }
+
+    addUser(client) {
+
+    }
+}
+
+module.exports = SetGameMan;
