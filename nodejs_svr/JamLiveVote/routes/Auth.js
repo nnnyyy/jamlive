b737348d735441c51c76ecdeac165c19ba6a3db7
nnnyyy <nnnyyy@nate.com>
2018-08-14 18:18:35 +0900

    인증 부분 수정 중

--- nodejs_svr/JamLiveVote/routes/Auth.js
+++ nodejs_svr/JamLiveVote/routes/Auth.js
@@ -3,25 +3,51 @@
  */
 var Log = require('./Log');
 var dbhelper = require('./dbhelper');
+var async = require('async');
 
 exports.login = function(req, res, next) {
+    async.waterfall(
+        [
+            async.apply(requestLogin, req),
+            requestGetItemList
+        ]
+        ,
+        function(err){
+            if( err == null ) {
+                res.json(0);
+            }
+            else {
+                res.json(err);
+            }
+        });
+}
+
+function requestLogin( req, callback ) {
     dbhelper.login(req.body.id, req.body.pw, function(json) {
         if( json.ret != 0 ){
-            res.json(json.ret);
+            //res.json(json.ret);
+            callback(json.ret);
             return;
         }
 
         req.session.username = json.id;
         req.session.usernick = json.nick;
         req.session.auth = json.auth;
-        res.json(json.ret);
+        //res.json(json.ret);
+        callback(null,req);
     })
+}
+
+function requestGetItemList(req, callback) {
+    req.session.items = [1];
+    callback(null);
 }
 
 exports.logout = function(req, res, next) {
     req.session.username = null;
     req.session.usernick = null;
     req.session.auth = null;
+    req.session.items = null;
     res.json({ret: 0});
 }
 
