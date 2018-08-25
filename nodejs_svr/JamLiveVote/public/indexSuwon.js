865e497593fe1018493f55636dbbedeb5875c3fc
nnnyyy <nnnyyy@nate.com>
2018-08-25 13:58:53 +0900

    텍스트 추가

--- nodejs_svr/JamLiveVote/public/indexSuwon.js
+++ nodejs_svr/JamLiveVote/public/indexSuwon.js
@@ -80,7 +80,7 @@ GlobalValue.prototype.onNextQuiz = function (data) {
 }
 
 function ChosungGameMan() {
-    this.questionTypeName = ['사전', '음식', '관용표현', '해산물', '날씨', '식물', '영화주인공', '나루토', '과일'];
+    this.questionTypeName = ['사전', '음식', '관용표현', '해산물', '날씨', '식물', '영화주인공', '나루토', '과일', '화가', '장소'];
     this.questionType = 0;
 }
 
