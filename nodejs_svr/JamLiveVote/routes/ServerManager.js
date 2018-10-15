1f94a9cb9a16d64367e4c25e137d24572ded55b5
nnnyyy <nnnyyy@nate.com>
2018-10-15 14:33:36 +0900

    포인트 어뷰징 제한

--- nodejs_svr/JamLiveVote/routes/ServerManager.js
+++ nodejs_svr/JamLiveVote/routes/ServerManager.js
@@ -1127,7 +1127,7 @@ function onSockVote(data) {
             client.lastVoteIdx =  data.idx;
             client.tLastClick = new Date();
 
-            if( servman.isLiveQuizTime() ) {
+            if( servman.isLiveQuizTime() && this.socketmap.count() >= 200 ) {
                 client.incActivePoint( 2 );
             }
 
